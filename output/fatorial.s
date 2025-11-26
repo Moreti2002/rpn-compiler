f5ae54eb5ec04206ac147018ace54adc6fc0e5af
moreti <joao.moreti2602@gmail.com>
2025-11-25 22:14:59 -0300

    Expandir fatorial.txt para calcular 1! ate 8!
    - Implementar calculo sequencial de todos os fatoriais (1 a 8)
    - Usar algoritmo com contador crescente (CONT) e multiplicador (FAT)
    - Cada fatorial reinicializa FAT=1 e CONT=1
    - Resultados esperados: 1, 2, 6, 24, 120, 720, 5040, 40320
    - Total: 32 expressoes, 128 instrucoes TAC
    - TAC otimizado: 72 instrucoes (43.8% reducao)
    - Assembly: 322 linhas

--- output/fatorial.s
+++ output/fatorial.s
@@ -220,16 +220,78 @@ programa_principal:
     push r17
     push r18
 
-    ldi r16, 5  ; t0 = 5
-    mov r17, r16  ; NUM = t0
-    ldi r18, 1  ; t1 = 1
-    mov r19, r18  ; FAT = t1
-    ldi r20, 1  ; t2 = 1
-    mov r21, r20  ; CONT = t2
-    mov r22, r19  ; copiar operando1
-    mul r22, r21  ; t3 = FAT * CONT
-    mov r22, r0  ; resultado em r0 (8-bit)
-    mov r19, r22  ; FAT = t3
+    ldi r16, 1  ; NUM = 1
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 1  ; CONT = 1
+L0:
+    ; ifFalse 1 goto L1 - sempre verdadeiro, não pula
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 2  ; CONT = 2
+    rjmp L0
+L1:
+    ldi r16, 2  ; NUM = 2
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 1  ; CONT = 1
+L2:
+    ; ifFalse 1 goto L3 - sempre verdadeiro, não pula
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 2  ; CONT = 2
+    rjmp L2
+L3:
+    ldi r16, 3  ; NUM = 3
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 1  ; CONT = 1
+L4:
+    ; ifFalse 1 goto L5 - sempre verdadeiro, não pula
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 2  ; CONT = 2
+    rjmp L4
+L5:
+    ldi r16, 4  ; NUM = 4
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 1  ; CONT = 1
+L6:
+    ; ifFalse 1 goto L7 - sempre verdadeiro, não pula
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 2  ; CONT = 2
+    rjmp L6
+L7:
+    ldi r16, 5  ; NUM = 5
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 1  ; CONT = 1
+L8:
+    ; ifFalse 1 goto L9 - sempre verdadeiro, não pula
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 2  ; CONT = 2
+    rjmp L8
+L9:
+    ldi r16, 6  ; NUM = 6
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 1  ; CONT = 1
+L10:
+    ; ifFalse 1 goto L11 - sempre verdadeiro, não pula
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 2  ; CONT = 2
+    rjmp L10
+L11:
+    ldi r16, 7  ; NUM = 7
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 1  ; CONT = 1
+L12:
+    ; ifFalse 1 goto L13 - sempre verdadeiro, não pula
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 2  ; CONT = 2
+    rjmp L12
+L13:
+    ldi r16, 8  ; NUM = 8
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 1  ; CONT = 1
+L14:
+    ; ifFalse 1 goto L15 - sempre verdadeiro, não pula
+    ldi r17, 1  ; FAT = 1
+    ldi r18, 2  ; CONT = 2
+    rjmp L14
+L15:
 
     pop r18
     pop r17
